b7256131157072aa02cb57a2e450280e74355264
akhild <deshmukh.akhil@gmail.com>
2018-06-29 15:22:40 +0530

    Added tooltip, and json parser

--- deps.js
+++ deps.js
@@ -3,6 +3,7 @@ projects = ["Non Reproducible", "DuplicateIssue", "Unsupported", "Feature Reques
             "MopSupplementFile", "OrderRepeaterConfig",                                 /* = 7 Configs*/
             "infra-gui-base", "gti_client", "chart_tools",                               /* = 9 internal repos */
             "ticker_venues", "secmaster",                                                /* = 12 different teas */
+            "Firewall"
             ]
 
 
@@ -22,30 +23,63 @@ edges = [
   {from: 6,  to: 10},
   {from: 4,  to: 11},
   {from: 6,  to: 12},
-  {from: 6,  to: 13}
+  {from: 6,  to: 13},
+  {from: 4,  to: 14}
 ]
 
-function addValue(visNodes, name, value) {
+function addValue(visNodes, network, name, value) {
     index = projects.indexOf(name);
-    visNodes.update([{id:index, value:value}]);
+    console.log("searching for", name, index)
+//    visNodes.update([{id:index, value:value}]);
+    network.on("afterDrawing", function(ctx) {
+        var ps = network.getPositions([index]);
+        ctx.fillText(value, ps[index].x-5, ps[index].y-5);
+    });
+}
+
+function addValueFor(visNodex, network, currentCounts, toolTip) {
+    indexes = [];
+    values = [];
+    titles = []
+
+    for (var key in currentCounts) {
+        indexes.push(projects.indexOf(key));
+        values.push(currentCounts[key]);
+        titles.push(toolTip[key])
+    }
+    console.log(indexes)
+    console.log(values)
+
+    network.on("afterDrawing", function(ctx) {
+        var ps = network.getPositions(indexes);
+        indexes.forEach(function(i, index) {
+            ctx.font = "20px Arial";
+            ctx.fillText(values[index], ps[i].x-5, ps[i].y-5);
+        });
+    });
+
+    indexes.forEach(function(index, i) {
+        visNodex.update([{id:index, title:titles[i]}]);
+    });
 }
 
 function initTypes(visNodes) {
     visNodes.update([
-        {id: 0, shape:"diamond"},
-        {id: 1, shape:"diamond"},
-        {id: 2, shape:"diamond"},
-        {id: 3, shape:"diamond"},
+        {id: 0, shape:"diamond", value: 2},
+        {id: 1, shape:"diamond", value: 2},
+        {id: 2, shape:"diamond", value: 2},
+        {id: 3, shape:"diamond", value: 2},
         {id: 4, shape:"dot", value:10},
-        {id: 5, shape:"dot", value: 5},
-        {id: 6, shape:"dot", value: 7},
-        {id: 7, shape:"square"},
-        {id: 8, shape:"square"},
-        {id: 9, shape:"dot"},
-        {id: 10, shape:"dot"},
-        {id: 11, shape:"dot"},
-        {id: 12, shape:"triangle"},
-        {id: 13, shape:"triangle"}
+        {id: 5, shape:"dot", value: 7},
+        {id: 6, shape:"dot", value: 8},
+        {id: 7, shape:"square", value: 2},
+        {id: 8, shape:"square", value: 2},
+        {id: 9, shape:"dot", value: 2},
+        {id: 10, shape:"dot", value: 2},
+        {id: 11, shape:"dot", value: 2},
+        {id: 12, shape:"triangle", value: 2},
+        {id: 13, shape:"triangle", value: 2},
+        {id: 14, shape:"triangle", value: 2}
     ])
 }
 
